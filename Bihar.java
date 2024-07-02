7b3423db34646b42e9624bf7a81978819915b628
Usha Kumari <ushakumari.xworkz@gmail.com>
2024-07-02 10:45:38 +0530

    java basics

---
+++ Bihar.java
@@ -0,0 +1,13 @@
+public class Bihar{
+	public static void main(String[] args)
+	{
+		System.out.println("Birthplace of Two Religions! : Bihar is the origin of the two biggest religions in the world, namely Buddhism and Jainism.");
+		System.out.println("World's Oldest University! : Nalanda library was set on fire by the army of Bakhtiyar Khilji.");
+		System.out.println("Bihar is a state in Eastern India. It is the third largest state by population");
+		System.out.println("Bihar's old name is 'Vihar");
+		System.out.println("Bihar Day (Bihar Diwas) is observed every year on March 22, marking the formation of the state of Bihar.");
+}
+}
+
+
+		
